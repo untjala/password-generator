424bb415faf1b314f055001402243075588a1388
Jala Unterreiner <jala.binbek@outlook.com>
2022-05-24 14:57:51 -0400

    added possible var to store user input

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function generatePassword() {
   var numbers = "0123456789";
   var special = "#$%&()*+,-./:;<=>?";
   var uppercase = lowercase.toUpperCase();
+  var possible = "";
 
   // Sets password length parameters 
   var input = parseInt(prompt("Choose a length between 8 and 128."));
@@ -14,41 +15,44 @@ function generatePassword() {
     //Returns invalid character length message 
   } else {
     alert("Please enter a length between 8 and 128.");
-    return "";
+  }
 
-    // Checks for lowercase letters
-    var lowercase = confirm("Would you like to use lowercase letters?");
-    if (lowercase === true) {
 
-    } else alert("OK")
+  // Checks for lowercase letters
+  var lowercase = confirm("Would you like to use lowercase letters?");
+  if (lowercase) {
+    possible += lowercase
   }
   //Checks for uppercase lettters
   var uppercase = confirm("Would you like to use uppercase letters?");
-  if (uppercase === true) {
+  if (uppercase) {
+    possible += uppercase
+  }
+  //Checks for special characters
+  var special = confirm("Would you like to use special characters?");
+  if (special) {
+    possible += special
+  }
+  //Checks for numbers
+  var numbers = confirm("Would you like to use numbers?");
+  if (numbers) {
+    possible += special
+  } else {
+    alert("Please use select one of the choices.");
+  }
 
-  } else alert("OK")
+  for (var i = 0; i < input; i++)
+  var possible= Math.floor(Math.random () * input.length);
+  console.log(possible)
+  
+  return "";
 }
-//Checks for special characters
-console.log(special)
-var special = confirm("Would you like to use special characters?");
-if (special === true) {
-
-} else alert("OK") 
-
-//Checks for numbers
-console.log(numbers)
-var numbers = confirm("Would you like to use numbers?");
-if (numbers === true) {
-
-} else {
-  alert("Please use special characters and or/numbers.");
-};
 // Password generation based on user choices
 // for (var i = 0; i > 8; i++)
 //   var characters = lowercase
 // var pasword = "";
-// var passwordMaker = Math.floor(Math.random() * password.length);
-// console.log(passwordMaker);
+// var passwordLength = Math.floor(Math.random() * password.length);
+// console.log(passwordLength);
 
 // Write password to the #password input
 function writePassword() {
